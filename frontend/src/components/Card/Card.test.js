9373662269fb41a07981dce6bcd49803d0a22163
bdelate <brendon.delate@gmail.com>
2018-05-10 09:18:42 +0200

    Added proptype validation

--- frontend/src/components/Card/Card.test.js
+++ frontend/src/components/Card/Card.test.js
@@ -8,10 +8,10 @@ import Card, { CardSource } from './Card';
 import TestBackend from 'react-dnd-test-backend';
 import { DragDropContext } from 'react-dnd';
 import TestUtils from 'react-dom/test-utils';
-import {configure, shallow, mount} from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 // Wraps a component in a DragDropContext that uses the dnd TestBackend.
 function wrapInTestContext(DecoratedComponent) {
@@ -32,6 +32,8 @@ it('can be dragged', () => {
       cardIndex={0}
       columnIndex={0}
       task={'This is a task'}
+      reorderCard={jest.fn()}
+      toggleCardCrud={jest.fn()}
     />
   );
 
