06e343c2b1ddd335a39bb8d0d38cbed5c35858e1
bdelate <brendon.delate@gmail.com>
2018-06-20 15:52:50 +0200

    CollapsedColumn Styled

--- kanban/frontend/src/components/CollapsedColumn/CollapsedColumn.js
+++ kanban/frontend/src/components/CollapsedColumn/CollapsedColumn.js
@@ -5,33 +5,44 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-
 const propTypes = {
   columnIndex: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   numCards: PropTypes.number.isRequired,
   toggleColumn: PropTypes.func.isRequired
-}
+};
 
 const ColumnContainer = styled.div`
+  display: flex;
+  align-items: center;
   padding: 10px;
   margin: 10px;
-  background-color: blue;
+  background-color: #00204a;
   text-orientation: sideways;
   writing-mode: vertical-lr;
+  .fas {
+    color: #fd5f00;
+  }
 `;
 
-const collapsedColumn = (props) => {
+const Header = styled.h3`
+  padding-top: 15px;
+  font-weight: 100;
+`;
+
+const collapsedColumn = props => {
   return (
     <ColumnContainer>
       <i
         title="Expand Column"
-        className="fas fa-expand"
+        className="fas fa-expand fa-lg"
         onClick={() => props.toggleColumn(props.columnIndex)}
-      ></i>
-      <span>{props.name}: {props.numCards} Tasks</span>
+      />
+      <Header>
+        {props.name}: {props.numCards} Tasks
+      </Header>
     </ColumnContainer>
-  )
+  );
 };
 
 collapsedColumn.propTypes = propTypes;
